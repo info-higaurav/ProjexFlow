bca24c5ad190ff1bed2975af7f7a279fda2d3ca4
info.higaurav <ai.higaurav@gmail.com>
2025-01-23 03:34:31 +0530

    feat: User schema added

---
+++ server/schema/users-schema/index.ts
@@ -0,0 +1,62 @@
+import {Schema , Document, model} from 'mongoose'
+
+enum UserRole {
+    admin = "admin",
+    user = "user"
+    
+}
+interface IUser extends Document {
+    profile : {
+        firstName : string
+        lastName : string
+    }
+    auth:{
+        email : string  
+        password : string
+        role : UserRole
+        accessToken? : string
+        refreshToken? : string
+    }
+}
+const userSchema = new Schema <IUser> ({
+    profile : {
+        firstName:{
+            type:String,
+            trim : true
+        },
+        lastName:{
+            type:String,
+            trim : true
+        }
+    },
+    auth:{
+        email:{
+            type:String,
+            unique : true,
+            required : true,
+            trim: true
+        },
+        role:{
+            type: String,
+            required:true,
+            enum : Object.values(UserRole),
+            default : "admin"
+        },
+        password:{
+            type:String,
+            required : true
+        },
+        accessToken:{
+            type:String,
+        },
+        refreshToken:{
+            type:String
+        }
+    }
+},{
+    timestamps : true
+})
+
+const User = model <IUser>("users", userSchema)
+
+export {User, IUser , UserRole}
